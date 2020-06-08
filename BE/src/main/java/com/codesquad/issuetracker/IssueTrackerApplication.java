5caa0f5cbf721e2529998a15b3c16c61fa2749ee
Byung Hyun Choi <choi8608@gmail.com>
2020-06-09 02:13:06 +0900

    [BE] chore :package: AppProperty 추가
    - AppProperty 추가

--- BE/src/main/java/com/codesquad/issuetracker/IssueTrackerApplication.java
+++ BE/src/main/java/com/codesquad/issuetracker/IssueTrackerApplication.java
@@ -1,11 +1,12 @@
 package com.codesquad.issuetracker;
 
+import com.codesquad.issuetracker.config.property.AppProperty;
 import com.codesquad.issuetracker.config.property.JwtProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties(JwtProperty.class)
+@EnableConfigurationProperties({JwtProperty.class, AppProperty.class})
 @SpringBootApplication
 public class IssueTrackerApplication {
 
