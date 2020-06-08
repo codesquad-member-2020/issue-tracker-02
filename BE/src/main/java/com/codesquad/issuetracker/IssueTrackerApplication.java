2fd4b83b6dfad08528e07dd6e4f35d78629c3ccc
Byung Hyun Choi <choi8608@gmail.com>
2020-06-08 19:11:33 +0900

    [BE] feat :fire: oauth2 Login 구현
    - http://13.125.127.249:8080/oauth2/authorization/github 로 접근시 로그인 구현
    - User 객체 형태의 json 으로 내려가도록 구현
    
    #3

--- BE/src/main/java/com/codesquad/issuetracker/IssueTrackerApplication.java
+++ BE/src/main/java/com/codesquad/issuetracker/IssueTrackerApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IssueTrackerApplication {
+public class IssueTrackerApplication{
 
   public static void main(String[] args) {
     SpringApplication.run(IssueTrackerApplication.class, args);
   }
-
 }
---
+++ BE/src/main/java/com/codesquad/issuetracker/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.codesquad.issuetracker.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(a ->
+        a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
+        .exceptionHandling(
+            e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .oauth2Login()
+        .defaultSuccessUrl("/api/oauth2/authorization")
+        .failureUrl("/api/oauth2/authorization");
+  }
+}
