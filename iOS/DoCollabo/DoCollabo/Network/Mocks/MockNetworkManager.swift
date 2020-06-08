39ebc534dc00fb0e5f2ac7c2d81a3149a8722c38
delma <sdy2856@gmail.com>
2020-06-08 21:24:09 +0900

    [iOS] feat :fire: github login 요청 보낼 네트워크 매니저 모크객체 생성
    #2

---
+++ iOS/DoCollabo/DoCollabo/Network/Mocks/MockNetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  DoCollabo
+//
+//  Created by delma on 2020/06/08.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+
+class MockNetworkManager {
+    func authenticateWithGithub() {
+        
+    }
+}
