99590bdffc2497e99e18ae3bc522ca011848deaf
Byung Hyun Choi <choi8608@gmail.com>
2020-06-10 15:15:14 +0900

    [BE] refactor :recycle: Issue 구조 개선
    - 필드 추가 및 생성자 추가

--- BE/src/main/java/com/codesquad/issuetracker/issue/data/Issue.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/data/Issue.java
@@ -1,12 +1,18 @@
 package com.codesquad.issuetracker.issue.data;
 
-import javax.persistence.Column;
+import com.codesquad.issuetracker.auth.data.User;
+import com.codesquad.issuetracker.issue.web.model.IssueQuery;
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @NoArgsConstructor
@@ -17,9 +23,39 @@ public class Issue {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(columnDefinition = "Boolean default false")
   private Boolean close;
   private String userId;
   private String title;
   private String content;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updateTimeAt;
+
+  @Builder
+  private Issue(Long id, Boolean close, String userId, String title, String content,
+      LocalDateTime createdAt, LocalDateTime updateTimeAt) {
+    this.id = id;
+    this.close = close;
+    this.userId = userId;
+    this.title = title;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.updateTimeAt = updateTimeAt;
+  }
+
+  public static Issue of(User user, IssueQuery query) {
+    return Issue.builder()
+        .userId(user.getUserId())
+        .title(query.getTitle())
+        .content(query.getContent())
+        .build();
+  }
+
+  @PrePersist
+  public void prePersist() {
+    this.close = false;
+  }
 }
