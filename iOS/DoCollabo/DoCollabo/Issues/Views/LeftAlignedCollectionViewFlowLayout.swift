cbf014bb1f09b7ec8a37c3bde1b1469e43274d9a
corykim0829 <corykim0829@gmail.com>
2020-06-10 18:48:48 +0900

    [iOS] feat :fire: BadgesCollectionView 왼쪽 정렬 Layout
    - 커스텀 CollectionViewFlowLayout 클래스를 구현하여 처리
    
    #22

---
+++ iOS/DoCollabo/DoCollabo/Issues/Views/LeftAlignedCollectionViewFlowLayout.swift
@@ -0,0 +1,30 @@
+//
+//  LeftAlignedCollectionViewFlowLayout.swift
+//  DoCollabo
+//
+//  Created by Cory Kim on 2020/06/10.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+final class LeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout {
+
+    override func layoutAttributesForElements(
+        in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let attributes = super.layoutAttributesForElements(in: rect)
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = -1.0
+        attributes?.forEach { layoutAttribute in
+            if layoutAttribute.frame.origin.y >= maxY {
+                leftMargin = sectionInset.left
+            }
+            
+            layoutAttribute.frame.origin.x = leftMargin
+            
+            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
+            maxY = max(layoutAttribute.frame.maxY , maxY)
+        }
+        return attributes
+    }
+}
