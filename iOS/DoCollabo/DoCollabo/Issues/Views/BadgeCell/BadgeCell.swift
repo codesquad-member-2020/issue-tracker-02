6057a7b117856c76bbd0c809f41579380076d0ed
delma <sdy2856@gmail.com>
2020-06-10 18:28:14 +0900

    [iOS] feat :fire: BadgeCell에 텍스트 지정할 수 있도록 동작 추가
    #22
    - horizontalPadding, verticalPadding 지정
    - label 더 두껍게  만듦

--- iOS/DoCollabo/DoCollabo/Issues/Views/BadgeCell/BadgeCell.swift
+++ iOS/DoCollabo/DoCollabo/Issues/Views/BadgeCell/BadgeCell.swift
@@ -10,10 +10,17 @@ import UIKit
 
 final class BadgeCell: UICollectionViewCell {
 
+    static let horizontalPadding: CGFloat = 18.0
+    static let verticalPadding: CGFloat = 10.0
+    
     @IBOutlet weak var background: UIView!
     @IBOutlet weak var label: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    func configureLabel(_ text: String) {
+        label.text = text
     }
 }
