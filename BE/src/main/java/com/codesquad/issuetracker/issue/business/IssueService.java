8e526d6b39890bcc96ddb7466646a944173e1f16
Byung Hyun Choi <choi8608@gmail.com>
2020-06-10 15:20:23 +0900

    [BE] feat :fire: Issue 추가하기
    - Issue 추가하기
    
    #19

--- BE/src/main/java/com/codesquad/issuetracker/issue/business/IssueService.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/business/IssueService.java
@@ -3,7 +3,9 @@ package com.codesquad.issuetracker.issue.business;
 import com.codesquad.issuetracker.auth.data.User;
 import com.codesquad.issuetracker.issue.data.Issue;
 import com.codesquad.issuetracker.issue.data.IssueRepository;
+import com.codesquad.issuetracker.issue.web.model.IssueQuery;
 import java.util.List;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,10 @@ public class IssueService {
 
   public List<Issue> getIssues(User user) {
     return issueRepository.findAllByUserIdEquals(user.getUserId());
+  }
+
+  @Transactional
+  public Issue create(User user, IssueQuery query) {
+    return issueRepository.save(Issue.of(user, query));
   }
 }
--- BE/src/main/java/com/codesquad/issuetracker/issue/web/IssueController.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/web/IssueController.java
@@ -3,6 +3,7 @@ package com.codesquad.issuetracker.issue.web;
 import com.codesquad.issuetracker.auth.data.User;
 import com.codesquad.issuetracker.issue.business.IssueService;
 import com.codesquad.issuetracker.issue.data.Issue;
+import com.codesquad.issuetracker.issue.web.model.IssueQuery;
 import com.codesquad.issuetracker.util.JwtUtil;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -10,11 +11,13 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/issue")
+@RequestMapping("/api/issues")
 @RestController
 public class IssueController {
 
@@ -27,5 +30,14 @@ public class IssueController {
   public List<Issue> getIssues(HttpServletRequest request) {
     User user = jwtUtil.parseUser(request.getHeader("Authorization"));
     return issueService.getIssues(user);
+  }
+
+  @ApiOperation(value = "Issue 를 추가합니다")
+  @ApiImplicitParam(name = "Authorization", required = true, paramType = "header")
+  @PostMapping
+  public Issue create(@RequestBody IssueQuery query, HttpServletRequest request) {
+    User user = jwtUtil.parseUser(request.getHeader("Authorization"));
+
+    return issueService.create(user, query);
   }
 }
