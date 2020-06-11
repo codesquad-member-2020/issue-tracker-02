a5f72c68aa52a440e0851332a52bee889354b49f
corykim0829 <corykim0829@gmail.com>
2020-06-11 18:36:29 +0900

    [iOS] feat :fire: hexString으로 UIColor 초기화
    - convenience init으로 구현
    
    #22

--- iOS/DoCollabo/DoCollabo/Extensions/UIColor+Hex.swift
+++ iOS/DoCollabo/DoCollabo/Extensions/UIColor+Hex.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 extension UIColor {
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if hexString.hasPrefix("#") {
+            scanner.currentIndex = scanner.string.index(after: scanner.currentIndex)
+        }
+        var color: UInt64 = 0
+        scanner.scanHexInt64(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
     func toHex() -> String {
         var red: CGFloat = 0
         var green: CGFloat = 0
