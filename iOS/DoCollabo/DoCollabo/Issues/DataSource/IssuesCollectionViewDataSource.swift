bc7275ad3924d5204c02a015e18d08e8ed2f8dcb
delma <sdy2856@gmail.com>
2020-06-11 21:47:18 +0900

    [iOS] feat :fire: IssuesCollectionViewDataSource 생성
    #30
    - NSObject, UICollectionViewDataSource, ViewModelBinding 채택

---
+++ iOS/DoCollabo/DoCollabo/Issues/DataSource/IssuesCollectionViewDataSource.swift
@@ -0,0 +1,48 @@
+//
+//  IssuesCollectionViewDataSource.swift
+//  DoCollabo
+//
+//  Created by delma on 2020/06/11.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+final class IssuesCollectionViewDataSource: NSObject, UICollectionViewDataSource, ViewModelBinding {
+
+    typealias Key = [Issue]
+    private var changedHandler: handler
+    private var issues: [Issue] {
+        didSet {
+            changedHandler(issues)
+        }
+    }
+    
+    init(issues: [Issue] = [], changedHandler: @escaping handler = { _ in }) {
+        self.issues = issues
+        self.changedHandler = changedHandler
+    }
+    
+    func updateNotify(handler: @escaping ([Issue]) -> Void) {
+        self.changedHandler = handler
+    }
+}
+
+extension IssuesCollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int) -> Int {
+        return issues.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: String(describing: IssueHorizontalCell.self),
+            for: indexPath) as! IssueHorizontalCell
+        let issue = issues[indexPath.item]
+        cell.configureCell(with: issue)
+        return cell
+    }
+}
