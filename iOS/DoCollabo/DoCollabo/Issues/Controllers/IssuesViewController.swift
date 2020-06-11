ef10e1348dfccda2898fa920dbc6adc1559cf6f2
delma <sdy2856@gmail.com>
2020-06-11 21:46:21 +0900

    [iOS] feat :fire: 서버로 이슈 데이터 목록 가져오도록 동작 구현 및 에러 핸들링
    #30

--- iOS/DoCollabo/DoCollabo/Issues/Controllers/IssuesViewController.swift
+++ iOS/DoCollabo/DoCollabo/Issues/Controllers/IssuesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 final class IssuesViewController: UIViewController {
 
@@ -16,20 +17,27 @@ final class IssuesViewController: UIViewController {
             titleHeaderView.configureTitle("이슈")
         }
     }
-    
     @IBOutlet weak var issuesCollectionView: IssuesCollectionView!
+    
+    private var issuesUseCase: UseCase!
+    private var dataSource: IssuesCollectionViewDataSource!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         fakeConfigureToken()
         configureCollectionViewDelegate()
+        configureUseCase()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         checkToken()
         titleHeaderBackgroundView.roundCorner(cornerRadius: 16.0)
+    }
+    
+    private func configureUseCase() {
+        issuesUseCase = UseCase(networkDispatcher: AF)
     }
     
     private func fakeConfigureToken() {
@@ -42,6 +50,7 @@ final class IssuesViewController: UIViewController {
             presentSignIn()
             return
         }
+        fetchIssues()
     }
     
     private func presentSignIn() {
@@ -52,6 +61,33 @@ final class IssuesViewController: UIViewController {
         }
         singInViewController.modalPresentationStyle = .fullScreen
         present(singInViewController, animated: true, completion: nil)
+    }
+    
+    private func fetchIssues() {
+        let request = FetchIssuesRequest().asURLRequest()
+        issuesUseCase.getResources(request: request, dataType: [Issue].self) { (result) in
+            switch result {
+            case .success(let issues):
+//                dataSource
+                break
+            case .failure(let error):
+                self.presentErrorAlert(error: error)
+            }
+        }
+    }
+    
+    private func presentErrorAlert(error: Error) {
+        let alertController = ErrorAlertController(
+            title: nil,
+            message: error.localizedDescription,
+            preferredStyle: .alert)
+        alertController.configure(actionTitle: "재요청") { (_) in
+            self.fetchIssues()
+        }
+        alertController.configure(actionTitle: "확인") { (_) in
+            return
+        }
+        self.present(alertController, animated: true)
     }
 }
 
