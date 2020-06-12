8801b1c52013c3a6191337d311db59620b55aa62
corykim0829 <corykim0829@gmail.com>
2020-06-12 14:46:07 +0900

    [iOS] feat :fire: titleHeaderBackgroundView scroll animation 구현
    #18

--- iOS/DoCollabo/DoCollabo/Issues/Controllers/IssuesViewController+ScrollAnimation.swift
+++ iOS/DoCollabo/DoCollabo/Issues/Controllers/IssuesViewController+ScrollAnimation.swift
@@ -11,33 +11,51 @@ import UIKit
 extension IssuesViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offsetY = scrollView.contentOffset.y
-        let currentHeight = titleHeaderViewHeightAnchor.constant
-        let diff = TitleHeaderView.stretchedHeight - TitleHeaderView.huggedHeight
+        let currentHeaderViewHeight = titleHeaderViewHeightAnchor.constant
+        let headerViewDiff = TitleHeaderView.stretchedHeight - TitleHeaderView.huggedHeight
+        let backgroundViewDiff = TitleHeaderBackgroundView.stretchedHeight - TitleHeaderBackgroundView.huggedHeight
         
         if offsetY > 0 {
-            if currentHeight > TitleHeaderView.huggedHeight {
+            if currentHeaderViewHeight > TitleHeaderView.huggedHeight {
+                let backgroundOffsetY = offsetY * (backgroundViewDiff / headerViewDiff)
+                titleHeaderBackgroundViewHeightAnchor.constant -= backgroundOffsetY
+            } else if currentHeaderViewHeight <= TitleHeaderView.huggedHeight {
+                guard currentHeaderViewHeight >= TitleHeaderView.huggedHeight else { return }
+                titleHeaderBackgroundViewHeightAnchor.constant = TitleHeaderBackgroundView.huggedHeight
+            }
+        } else if offsetY < 0 {
+            if currentHeaderViewHeight < TitleHeaderView.stretchedHeight {
+                let backgroundOffsetY = offsetY * (backgroundViewDiff / headerViewDiff)
+                titleHeaderBackgroundViewHeightAnchor.constant -= backgroundOffsetY
+            } else if currentHeaderViewHeight >= TitleHeaderView.stretchedHeight {
+                guard currentHeaderViewHeight <= TitleHeaderView.stretchedHeight else { return }
+                titleHeaderBackgroundViewHeightAnchor.constant = TitleHeaderBackgroundView.stretchedHeight
+            }
+        }
+        
+        if offsetY > 0 {
+            if currentHeaderViewHeight > TitleHeaderView.huggedHeight {
                 titleHeaderViewHeightAnchor.constant -= offsetY
-                let heightOffset = TitleHeaderView.stretchedHeight - currentHeight
-                let alpha = (heightOffset / diff)
+                let heightOffset = TitleHeaderView.stretchedHeight - currentHeaderViewHeight
+                let alpha = (heightOffset / headerViewDiff)
                 titleHeaderView.titleLabel.alpha = 1.0 - alpha
                 titleHeaderView.smallTitleLabel.alpha = alpha
-            } else if currentHeight <= TitleHeaderView.huggedHeight {
+            } else if currentHeaderViewHeight <= TitleHeaderView.huggedHeight {
                 titleHeaderView.hugged()
-                guard currentHeight != TitleHeaderView.huggedHeight else { return }
                 titleHeaderViewHeightAnchor.constant = TitleHeaderView.huggedHeight
                 UIView.animate(withDuration: 0.1) {
                     self.view.layoutIfNeeded()
                 }
             }
         } else if offsetY < 0 {
-            if currentHeight < TitleHeaderView.stretchedHeight {
+            if currentHeaderViewHeight < TitleHeaderView.stretchedHeight {
                 titleHeaderViewHeightAnchor.constant -= offsetY
-                let heightOffset = currentHeight - TitleHeaderView.huggedHeight
-                let alpha = (heightOffset / diff)
+                let heightOffset = currentHeaderViewHeight - TitleHeaderView.huggedHeight
+                let alpha = (heightOffset / headerViewDiff)
                 titleHeaderView.titleLabel.alpha = alpha
                 titleHeaderView.smallTitleLabel.alpha = 1.0 - alpha
-            } else if currentHeight >= TitleHeaderView.stretchedHeight {
-                guard currentHeight != TitleHeaderView.stretchedHeight else { return }
+            } else if currentHeaderViewHeight >= TitleHeaderView.stretchedHeight {
+                titleHeaderView.stretched()
                 titleHeaderViewHeightAnchor.constant = TitleHeaderView.stretchedHeight
                 UIView.animate(withDuration: 0.1) {
                     self.view.layoutIfNeeded()
@@ -47,26 +65,28 @@ extension IssuesViewController: UIScrollViewDelegate {
     }
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        let currentHeight = titleHeaderViewHeightAnchor.constant
+        let currentHeaderViewHeight = titleHeaderViewHeightAnchor.constant
         
-        guard currentHeight != TitleHeaderView.huggedHeight ||
-            currentHeight != TitleHeaderView.stretchedHeight
+        guard currentHeaderViewHeight != TitleHeaderView.huggedHeight ||
+            currentHeaderViewHeight != TitleHeaderView.stretchedHeight
         else {
             return
         }
         
         let maximumOfDiff = TitleHeaderView.stretchedHeight - TitleHeaderView.huggedHeight
-        let diff = currentHeight - TitleHeaderView.huggedHeight
+        let diff = currentHeaderViewHeight - TitleHeaderView.huggedHeight
         let diffOffsetRatio = diff / maximumOfDiff
         if diffOffsetRatio < 0.5 {
             titleHeaderViewHeightAnchor.constant = TitleHeaderView.huggedHeight
-            UIView.animate(withDuration: 0.3) {
+            titleHeaderBackgroundViewHeightAnchor.constant = TitleHeaderBackgroundView.huggedHeight
+            UIView.animate(withDuration: 0.5) {
                 self.titleHeaderView.hugged()
                 self.view.layoutIfNeeded()
             }
         } else {
             titleHeaderViewHeightAnchor.constant = TitleHeaderView.stretchedHeight
-            UIView.animate(withDuration: 0.3) {
+            titleHeaderBackgroundViewHeightAnchor.constant = TitleHeaderBackgroundView.stretchedHeight
+            UIView.animate(withDuration: 0.5) {
                 self.titleHeaderView.stretched()
                 self.view.layoutIfNeeded()
             }
