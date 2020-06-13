a4b5a394dae2921999b929c25a856741410c2d62
delma <sdy2856@gmail.com>
2020-06-13 23:50:04 +0900

    [iOS] feat :fire: AddView 생성
    #48

---
+++ iOS/DoCollabo/DoCollabo/Views/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  DoCollabo
+//
+//  Created by delma on 2020/06/13.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class AddView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
