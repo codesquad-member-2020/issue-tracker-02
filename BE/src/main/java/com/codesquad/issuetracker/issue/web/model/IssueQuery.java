96be11bb8ad454ebad244f97cab1c7cb10687d96
Byung Hyun Choi <choi8608@gmail.com>
2020-06-15 00:37:48 +0900

    [BE] refactor :recycle: IssueQuery 개선
    - Lombok 을 통해 코드 개선

--- BE/src/main/java/com/codesquad/issuetracker/issue/web/model/IssueQuery.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/web/model/IssueQuery.java
@@ -1,14 +1,22 @@
 package com.codesquad.issuetracker.issue.web.model;
 
-
-import com.codesquad.issuetracker.label.data.Label;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class IssueQuery {
 
-  private String title;
-  private String description;
-  private List<Label> labels;
+  private final String title;
+  private final String description;
+  private final Set<Long> idOfLabels;
+
+  public static IssueQuery of(String title, String description, LinkedHashSet<Long> idOfLabels) {
+    return new IssueQuery(title, description, idOfLabels);
+  }
 }
