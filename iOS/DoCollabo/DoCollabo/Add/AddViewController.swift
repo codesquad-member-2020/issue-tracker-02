84689ced62abee21f52320a571ab1d2b61abbcdd
delma <sdy2856@gmail.com>
2020-06-15 19:50:52 +0900

    [iOS] feat :fire: AddViewController 생성 및 오토레이아웃 조정
    #48

---
+++ iOS/DoCollabo/DoCollabo/Add/AddViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddViewController.swift
+//  DoCollabo
+//
+//  Created by delma on 2020/06/15.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var contentsView: UIView!
+    @IBOutlet weak var contentsStackView: UIStackView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UILabel!
+    @IBOutlet weak var emptyView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        configureUI()
+    }
+    
+    private func configureUI() {
+        contentsView.roundCorner(cornerRadius: 12.0)
+    }
+}
