65acf098f0b4a93b53f9639abc5b45f5be3bc9e1
Byung Hyun Choi <choi8608@gmail.com>
2020-06-16 00:07:33 +0900

    [BE] chore :package: 에러 메세지 추가
    - NOT_EXIST_MILESTONE = "존재하지 않는 Milstone 입니다";

--- BE/src/main/java/com/codesquad/issuetracker/exception/ErrorMessage.java
+++ BE/src/main/java/com/codesquad/issuetracker/exception/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
 
   public static String ANOTHER_USER_ISSUE = "다른 유저의 Issue 입니다";
   public static String NOT_EXIST_LABEL = "존재하지 않는 Label 입니다";
+  public static String NOT_EXIST_MILESTONE = "존재하지 않는 Milstone 입니다";
 }
