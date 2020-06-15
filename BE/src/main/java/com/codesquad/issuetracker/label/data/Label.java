46b2b31100182e0286780fca33fe67d6f8de487d
Byung Hyun Choi <choi8608@gmail.com>
2020-06-15 17:19:34 +0900

    [BE] refactor :recycle: Label 개선
    - @ManyToMany 를 만들기 위한 중간 Entity, IssueLabelRelation 정의
      - 순환 참조를 막기 위해 DTO 를 만드는 extractMainInform() 추가
    
    #26

--- BE/src/main/java/com/codesquad/issuetracker/label/data/Label.java
+++ BE/src/main/java/com/codesquad/issuetracker/label/data/Label.java
@@ -1,11 +1,18 @@
 package com.codesquad.issuetracker.label.data;
 
+import com.codesquad.issuetracker.issue.data.IssueLabelRelation;
 import com.codesquad.issuetracker.label.web.model.LabelQuery;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,9 +34,22 @@ public class Label {
   private String description;
   private String color;
 
+  @JsonInclude(Include.NON_NULL)
+  @OneToMany(mappedBy = "label", fetch = FetchType.EAGER)
+  private List<IssueLabelRelation> issueLabelRelations = new ArrayList<>();
+
   public static Label of(Long id) {
     return Label.builder()
         .id(id)
+        .build();
+  }
+
+  public static Label extractMainInform(Label label) {
+    return Label.builder()
+        .id(label.getId())
+        .title(label.getTitle())
+        .description(label.getDescription())
+        .color(label.getColor())
         .build();
   }
 
