ba0009b85d377c8bbb8f0b7517c7b372f9e3fef3
Byung Hyun Choi <choi8608@gmail.com>
2020-06-15 18:44:34 +0900

    [BE] refactor :recycle: User 에 NoArgsConstructor 추가
    - 기본 생성자가 있어야 작동합니다

--- BE/src/main/java/com/codesquad/issuetracker/auth/data/User.java
+++ BE/src/main/java/com/codesquad/issuetracker/auth/data/User.java
@@ -3,11 +3,13 @@ package com.codesquad.issuetracker.auth.data;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
   private String nodeId;
