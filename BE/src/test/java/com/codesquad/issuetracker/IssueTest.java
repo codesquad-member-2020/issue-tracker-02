d656467145aeb632ddb49abf666d127f2db0eb36
Byung Hyun Choi <choi8608@gmail.com>
2020-06-15 17:16:38 +0900

    [BE] test :white_check_mark: Issue 테스트 개선
    - 개선된 구조에 대응하는 형태로 테스트 개선

--- BE/src/test/java/com/codesquad/issuetracker/IssueTest.java
+++ BE/src/test/java/com/codesquad/issuetracker/IssueTest.java
@@ -1,12 +1,23 @@
 package com.codesquad.issuetracker;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import com.codesquad.issuetracker.auth.data.User;
+import com.codesquad.issuetracker.exception.ErrorMessage;
+import com.codesquad.issuetracker.exception.NotAllowedException;
 import com.codesquad.issuetracker.issue.business.IssueService;
 import com.codesquad.issuetracker.issue.data.Issue;
 import com.codesquad.issuetracker.issue.data.IssueRepository;
+import com.codesquad.issuetracker.issue.web.model.IssueQuery;
 import com.codesquad.issuetracker.issue.web.model.IssueView;
+import com.google.common.primitives.Longs;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,9 +34,6 @@ public class IssueTest {
 
     @Autowired
     private IssueService issueService;
-
-    @Autowired
-    private IssueRepository issueRepository;
 
     @Nested
     @DisplayName("Issue 를 가져옵니다")
@@ -55,120 +63,139 @@ public class IssueTest {
         assertThat(findIssue).isNotNull();
       }
     }
-//
-//    @Nested
-//    @DisplayName("Issue 를 추가합니다")
-//    @Transactional
-//    @SpringBootTest
-//    public class CreateTest {
-//
-//      private User sampleUser;
-//      private IssueQuery sampleIssueQuery;
-//
-//      @BeforeEach
-//      private void beforeEach() {
-//        sampleUser = User.builder()
-//            .nodeId("MDQ6VXNlcjU1NzIyMTg2")
-//            .userId("Hyune-c")
-//            .avatarUrl("https://avatars1.githubusercontent.com/u/55722186?v=4").build();
-//      }
-//
-//      @DisplayName("Label 이 없는 IssueQuery 로")
-//      @Test
-//      public void createWithNoLabel() {
-//        // given
-//        sampleIssueQuery = IssueQuery.of("Hyune-c 1"
-//            , "Hyune-c contents1\\nHyune-c contents1\\nHyune-c contents1", new LinkedHashSet<>());
-//
-//        // when
-//        Issue savedIssue = issueService.create(sampleUser, sampleIssueQuery);
-//
-//        // then
-//        Optional<Issue> findOptionalIssue = issueRepository.findById(savedIssue.getId());
-//        assertThat(findOptionalIssue.orElseThrow(NoSuchElementException::new).getId())
-//            .isEqualTo(savedIssue.getId());
-//      }
-//
-//      @DisplayName("존재하는 Label id 를 가지고 있는 IssueQuery 로")
-//      @Test
-//      public void createWithLabel() {
-//        // given
-//        LinkedHashSet<Long> idOfLabels = new LinkedHashSet<>(Longs.asList(4, 5, 1));
-//        sampleIssueQuery = IssueQuery.of("Hyune-c 1"
-//            , "Hyune-c contents1\\nHyune-c contents1\\nHyune-c contents1", idOfLabels);
-//
-//        // when
-//        Issue savedIssue = issueService.create(sampleUser, sampleIssueQuery);
-//
-//        // then
-//        Optional<Issue> findOptionalIssue = issueRepository.findById(savedIssue.getId());
-//        assertThat(findOptionalIssue.orElseThrow(NoSuchElementException::new).getId())
-//            .isEqualTo(savedIssue.getId());
-//      }
-//
-//      @DisplayName("존재하지 않는 Label id 를 가지고 있는 IssueQuery 로")
-//      @Test
-//      public void createWithWrongLabel() {
-//        // given
-//        LinkedHashSet<Long> idOfLabels = new LinkedHashSet<>(Longs.asList(4, 15, 1));
-//        sampleIssueQuery = IssueQuery.of("Hyune-c 1"
-//            , "Hyune-c contents1\\nHyune-c contents1\\nHyune-c contents1", idOfLabels);
-//
-//        // when
-//        assertThatExceptionOfType(NoSuchElementException.class)
-//            .isThrownBy(() -> issueService.create(sampleUser, sampleIssueQuery))
-//            .withMessage(ErrorMessage.NOT_EXIST_LABEL);
-//
-//        // then
-//      }
-//    }
-//
-//    @Nested
-//    @DisplayName("Issue 를 삭제합니다")
-//    @Transactional
-//    @SpringBootTest
-//    public class DeleteTest {
-//
-//      private User sampleUser;
-//
-//      @BeforeEach
-//      private void beforeEach() {
-//        sampleUser = User.builder()
-//            .nodeId("MDQ6VXNlcjU1NzIyMTg2")
-//            .userId("Hyune-c")
-//            .avatarUrl("https://avatars1.githubusercontent.com/u/55722186?v=4").build();
-//      }
-//
-//      @DisplayName("맞는 User 의")
-//      @Transactional
-//      @Test
-//      void deleteOwnIssue() {
-//        // given
-//
-//        // when
-//        issueService.delete(sampleUser, 9L);
-//        Optional<Issue> deletedOptionalIssue = issueRepository.findById(9L);
-//
-//        // then
-//        assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> {
-//          deletedOptionalIssue.orElseThrow(NoSuchElementException::new);
-//        });
-//      }
-//
-//      @DisplayName("다른 User 의")
-//      @Transactional
-//      @Test
-//      void deleteOtherIssue() {
-//        // given
-//
-//        // when
-//        assertThatExceptionOfType(NotAllowedException.class).isThrownBy(() -> {
-//          issueService.delete(sampleUser, 1L);
-//        }).withMessage(ErrorMessage.ANOTHER_USER_ISSUE);
-//
-//        // then
-//      }
-//    }
-//  }
+
+    @Nested
+    @DisplayName("Issue 를 추가합니다")
+    @Transactional
+    @SpringBootTest
+    public class CreateTest {
+
+      @Autowired
+      private IssueRepository issueRepository;
+
+      private User sampleUser;
+      private IssueQuery sampleIssueQuery;
+
+      @BeforeEach
+      private void beforeEach() {
+        sampleUser = User.builder()
+            .nodeId("MDQ6VXNlcjU1NzIyMTg2")
+            .userId("Hyune-c")
+            .avatarUrl("https://avatars1.githubusercontent.com/u/55722186?v=4")
+            .build();
+      }
+
+      @DisplayName("Label 이 없는 IssueQuery 로")
+      @Test
+      public void createWithNoLabel() {
+        // given
+        sampleIssueQuery = IssueQuery.builder()
+            .title("Hyune-c 1")
+            .description("Hyune-c contents1\\nHyune-c contents1\\nHyune-c contents1")
+            .build();
+
+        // when
+        IssueView savedIssue = issueService.create(sampleUser, sampleIssueQuery);
+
+        // then
+        Optional<Issue> findOptionalIssue = issueRepository.findById(savedIssue.getId());
+        Issue findIssue = findOptionalIssue.orElseThrow(NoSuchElementException::new);
+
+        assertThat(findIssue.getId())
+            .isEqualTo(savedIssue.getId());
+      }
+
+      @DisplayName("존재하는 Label id 를 가지고 있는 IssueQuery 로")
+      @Test
+      public void createWithLabel() {
+        // given
+        LinkedHashSet<Long> idOfLabels = new LinkedHashSet<>(Longs.asList(4, 5, 1));
+        sampleIssueQuery = IssueQuery.builder()
+            .title("Hyune-c 1")
+            .description("Hyune-c contents1\\nHyune-c contents1\\nHyune-c contents1")
+            .idOfLabels(idOfLabels)
+            .build();
+
+        // when
+        IssueView savedIssue = issueService.create(sampleUser, sampleIssueQuery);
+
+        // then
+        Optional<Issue> findOptionalIssue = issueRepository.findById(savedIssue.getId());
+        Issue findIssue = findOptionalIssue.orElseThrow(NoSuchElementException::new);
+
+        assertThat(findIssue.getId())
+            .isEqualTo(savedIssue.getId());
+        assertThat(findIssue.getIssueLabelRelations().size())
+            .isEqualTo(idOfLabels.size());
+      }
+
+      @DisplayName("존재하지 않는 Label id 를 가지고 있는 IssueQuery 로")
+      @Test
+      public void createWithWrongLabel() {
+        // given
+        LinkedHashSet<Long> idOfLabels = new LinkedHashSet<>(Longs.asList(4, 15, 1));
+        sampleIssueQuery = IssueQuery.builder()
+            .title("Hyune-c 1")
+            .description("Hyune-c contents1\\nHyune-c contents1\\nHyune-c contents1")
+            .idOfLabels(idOfLabels)
+            .build();
+
+        // when
+        assertThatExceptionOfType(NoSuchElementException.class)
+            .isThrownBy(() -> issueService.create(sampleUser, sampleIssueQuery))
+            .withMessage(ErrorMessage.NOT_EXIST_LABEL);
+
+        // then
+      }
+    }
+
+    @Nested
+    @DisplayName("Issue 를 삭제합니다")
+    @Transactional
+    @SpringBootTest
+    public class DeleteTest {
+
+      @Autowired
+      private IssueRepository issueRepository;
+
+      private User sampleUser;
+
+      @BeforeEach
+      private void beforeEach() {
+        sampleUser = User.builder()
+            .nodeId("MDQ6VXNlcjU1NzIyMTg2")
+            .userId("Hyune-c")
+            .avatarUrl("https://avatars1.githubusercontent.com/u/55722186?v=4").build();
+      }
+
+      @DisplayName("맞는 User 의")
+      @Test
+      void deleteOwnIssue() {
+        // given
+
+        // when
+        issueService.delete(sampleUser, 9L);
+
+        // then
+        Optional<Issue> deletedOptionalIssue = issueRepository.findById(9L);
+
+        assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> {
+          deletedOptionalIssue.orElseThrow(NoSuchElementException::new);
+        });
+      }
+
+      @DisplayName("다른 User 의")
+      @Test
+      void deleteOtherIssue() {
+        // given
+
+        // when
+        assertThatExceptionOfType(NotAllowedException.class).isThrownBy(() -> {
+          issueService.delete(sampleUser, 1L);
+        }).withMessage(ErrorMessage.ANOTHER_USER_ISSUE);
+
+        // then
+      }
+    }
   }
 }
