b27eab8950d1f2ec4e232a31a97547f2b0f2232f
Byung Hyun Choi <choi8608@gmail.com>
2020-06-16 22:39:33 +0900

    [BE] refactor :recycle: Issue 관련 로직 개선
    - IssueService
      - 영속성 관리를 끊으면서 Create 시 수동 작업이 필요합니다
    - IssueView
      - 매핑 관계 변경으로 Issue 내부에서 가져오던 것을 외부에서 주입받도록 수정

--- BE/src/main/java/com/codesquad/issuetracker/issue/business/IssueService.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/business/IssueService.java
@@ -5,13 +5,20 @@ import com.codesquad.issuetracker.exception.ErrorMessage;
 import com.codesquad.issuetracker.exception.NotAllowedException;
 import com.codesquad.issuetracker.issue.data.Issue;
 import com.codesquad.issuetracker.issue.data.IssueRepository;
+import com.codesquad.issuetracker.issue.data.relation.IssueLabelRelation;
+import com.codesquad.issuetracker.issue.data.relation.IssueLabelRelationRepository;
+import com.codesquad.issuetracker.issue.data.relation.IssueMilestoneRelation;
+import com.codesquad.issuetracker.issue.data.relation.IssueMilestoneRelationRepository;
 import com.codesquad.issuetracker.issue.web.model.IssueQuery;
 import com.codesquad.issuetracker.issue.web.model.IssueView;
 import com.codesquad.issuetracker.label.data.Label;
 import com.codesquad.issuetracker.label.data.LabelRepository;
+import com.codesquad.issuetracker.milestone.data.Milestone;
+import com.codesquad.issuetracker.milestone.data.MilestoneRepository;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.stream.Collectors;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -23,27 +30,63 @@ public class IssueService {
 
   private final IssueRepository issueRepository;
   private final LabelRepository labelRepository;
+  private final MilestoneRepository milestoneRepository;
+
+  private final IssueLabelRelationRepository issueLabelRelationRepository;
+  private final IssueMilestoneRelationRepository issueMilestoneRelationRepository;
+
+  private LinkedHashSet<Label> extractLabels(Issue issue) {
+    return new LinkedHashSet<>(labelRepository.findAllById(issue.idOfLabels()));
+  }
+
+  private LinkedHashSet<Milestone> extractMilestones(Issue issue) {
+    return new LinkedHashSet<>(milestoneRepository.findAllById(issue.idOfMilestones()));
+  }
+
+  private Boolean isValidLabelIds(Set<Long> idOfLabels) {
+    return idOfLabels.size() == labelRepository.findAllById(idOfLabels).size();
+  }
+
+  private Boolean isValidMilestoneIds(Set<Long> idOfMilestones) {
+    return idOfMilestones.size() == milestoneRepository.findAllById(idOfMilestones).size();
+  }
 
   public List<IssueView> getIssues() {
     List<Issue> findIssues = issueRepository.findAll();
-    return IssueView.toList(findIssues);
+    return findIssues.stream()
+        .map(issue -> IssueView.from(issue, extractLabels(issue), extractMilestones(issue)))
+        .collect(Collectors.toList());
   }
 
   public IssueView getIssue(Long issueId) {
     Issue findIssue = issueRepository.findById(issueId).orElseThrow(NoSuchElementException::new);
-    return IssueView.from(findIssue);
+    return IssueView.from(findIssue, extractLabels(findIssue), extractMilestones(findIssue));
   }
 
   @Transactional
   public IssueView create(User user, IssueQuery query) {
-    LinkedHashSet<Label> labels = query.getIdOfLabels().stream()
-        .map(id -> labelRepository.findById(id)
-            .orElseThrow(() -> new NoSuchElementException(ErrorMessage.NOT_EXIST_LABEL)))
-        .collect(Collectors.toCollection(LinkedHashSet::new));
-    Issue issue = Issue.from(user, query, labels);
-    Issue savedIssue = issueRepository.save(issue);
+    Set<Long> idOfLabels = query.getIdOfLabels();
+    Set<Long> idOfMilestones = query.getIdOfMilestones();
 
-    return IssueView.from(savedIssue);
+    if (!isValidLabelIds(idOfLabels)) {
+      throw new NoSuchElementException(ErrorMessage.NOT_EXIST_LABEL);
+    }
+
+    if (!isValidMilestoneIds(idOfMilestones)) {
+      throw new NoSuchElementException(ErrorMessage.NOT_EXIST_MILESTONE);
+    }
+
+    Issue issue = Issue.from(user, query);
+    Issue savedIssue = issueRepository.save(issue);
+    savedIssue.addAllLabel(idOfLabels.stream()
+        .map(labelId -> issueLabelRelationRepository.save(IssueLabelRelation.of(issue, labelId)))
+        .collect(Collectors.toSet()));
+    savedIssue.addAllMilestone(idOfMilestones.stream()
+        .map(milestoneId -> issueMilestoneRelationRepository
+            .save(IssueMilestoneRelation.of(issue, milestoneId)))
+        .collect(Collectors.toSet()));
+
+    return IssueView.from(savedIssue, extractLabels(savedIssue), extractMilestones(savedIssue));
   }
 
   @Transactional
--- BE/src/main/java/com/codesquad/issuetracker/issue/web/model/IssueView.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/web/model/IssueView.java
@@ -4,9 +4,7 @@ import com.codesquad.issuetracker.issue.data.Issue;
 import com.codesquad.issuetracker.label.data.Label;
 import com.codesquad.issuetracker.milestone.data.Milestone;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,15 +25,7 @@ public class IssueView {
   private final LocalDateTime createdAt;
   private final LocalDateTime updateTimeAt;
 
-  public static IssueView from(Issue issue) {
-    Set<Label> labels = issue.getIssueLabelRelations().stream()
-        .map(issueLabelRelation -> Label.extractMainInform(issueLabelRelation.getLabel()))
-        .collect(Collectors.toSet());
-    Set<Milestone> milestones = issue.getIssueMilestoneRelations().stream()
-        .map(issueMilestoneRelation -> Milestone
-            .extractMainInform(issueMilestoneRelation.getMilestone()))
-        .collect(Collectors.toSet());
-
+  public static IssueView from(Issue issue, Set<Label> labels, Set<Milestone> milestones) {
     return IssueView.builder()
         .id(issue.getId())
         .close(issue.getClose())
@@ -47,11 +37,5 @@ public class IssueView {
         .createdAt(issue.getCreatedAt())
         .updateTimeAt(issue.getUpdateTimeAt())
         .build();
-  }
-
-  public static List<IssueView> toList(List<Issue> issues) {
-    return issues.stream()
-        .map(IssueView::from)
-        .collect(Collectors.toList());
   }
 }
