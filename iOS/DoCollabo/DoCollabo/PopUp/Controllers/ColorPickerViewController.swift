37ebdc2125ee62edbe69777077ec4bbfec615b25
delma <sdy2856@gmail.com>
2020-06-20 02:07:48 +0900

    [iOS] feat :fire: ColorPickerViewController에서 선택한 컬러 저장
    #88

--- iOS/DoCollabo/DoCollabo/PopUp/Controllers/ColorPickerViewController.swift
+++ iOS/DoCollabo/DoCollabo/PopUp/Controllers/ColorPickerViewController.swift
@@ -12,27 +12,45 @@ import Colorful
 final class ColorPickerViewController: PopUpViewController {
     
     private var palette: ColorPicker!
+    private var selectedColor: String!
     
     func configureColorPickerView() {
         configure()
         configurePalette()
         hideSecondaryButton()
+        configureSecondLevelBackgroundView()
     }
     
-    func configurePalette() {
+    private func configurePalette() {
         palette = ColorPicker()
         palette.backgroundColor = .white
         palette.translatesAutoresizingMaskIntoConstraints = false
         palette.set(
-            color: UIColor(displayP3Red: CGFloat.random(in: 0...1.0), green: CGFloat.random(in: 0...1.0), blue: CGFloat.random(in: 0...1.0), alpha: 1),
+            color: UIColor(displayP3Red: CGFloat.random(in: 0...1.0),
+                           green: CGFloat.random(in: 0...1.0),
+                           blue: CGFloat.random(in: 0...1.0), alpha: 1),
             colorSpace: .sRGB)
         configureContentView(palette)
-        configureSecondLevelBackgroundView()
+        configureSelectedColorString()
+    }
+    
+    private func configureSelectedColorString() {
+        selectedColor = palette.color.hexString
+        palette.addTarget(self, action: #selector(self.handleColorChanged(_:)), for: .valueChanged)
+        handleColorChanged(palette)
+    }
+    
+    @objc func handleColorChanged(_ palette: ColorPicker) {
+        selectedColor = palette.color.hexString
     }
 }
 
 extension ColorPickerViewController {
     override func cancelButtonDidTap() {
+        dismiss(animated: false, completion: nil)
+    }
+    
+    override func submitButtonDidTap() {
         dismiss(animated: false, completion: nil)
     }
 }
