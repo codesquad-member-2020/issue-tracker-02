cef70e79f1fb1a5edd3f8d92b55e1ca0cb208a17
delma <sdy2856@gmail.com>
2020-06-22 22:36:08 +0900

    [iOS]  feat :fire: User 모델 생성
    #129

---
+++ iOS/DoCollabo/DoCollabo/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  DoCollabo
+//
+//  Created by delma on 2020/06/22.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var userID: String
+    var profileImage: String
+}
