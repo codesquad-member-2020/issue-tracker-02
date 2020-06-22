819717bd65a2462442918fee08f4404356608763
Byung Hyun Choi <choi8608@gmail.com>
2020-06-22 17:30:13 +0900

    [BE] feat :fire: reply 를 가져올 수 있도록 로직 추가
    - reply 를 가져올 수 있도록 로직 추가
    - data.sql 수정

--- BE/src/main/java/com/codesquad/issuetracker/issue/web/model/IssueView.java
+++ BE/src/main/java/com/codesquad/issuetracker/issue/web/model/IssueView.java
@@ -1,9 +1,11 @@
 package com.codesquad.issuetracker.issue.web.model;
 
 import com.codesquad.issuetracker.issue.data.Issue;
+import com.codesquad.issuetracker.issue.data.Reply;
 import com.codesquad.issuetracker.label.data.Label;
 import com.codesquad.issuetracker.milestone.data.Milestone;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -22,6 +24,10 @@ public class IssueView {
   private final String description;
   private final Set<Label> labels;
   private final Set<Milestone> milestones;
+
+  @Builder.Default
+  private final Set<Reply> replies = new HashSet<>();
+
   private final LocalDateTime createdAt;
   private final LocalDateTime updateTimeAt;
 
@@ -34,6 +40,22 @@ public class IssueView {
         .description(issue.getDescription())
         .labels(labels)
         .milestones(milestones)
+        .createdAt(issue.getCreatedAt())
+        .updateTimeAt(issue.getUpdateTimeAt())
+        .build();
+  }
+
+  public static IssueView from(Issue issue, Set<Label> labels, Set<Milestone> milestones,
+      Set<Reply> replies) {
+    return IssueView.builder()
+        .id(issue.getId())
+        .close(issue.getClose())
+        .userId(issue.getUserId())
+        .title(issue.getTitle())
+        .description(issue.getDescription())
+        .labels(labels)
+        .milestones(milestones)
+        .replies(replies)
         .createdAt(issue.getCreatedAt())
         .updateTimeAt(issue.getUpdateTimeAt())
         .build();
