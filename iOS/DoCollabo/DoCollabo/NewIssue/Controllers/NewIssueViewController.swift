0f4a4791b1f56dd9bf2bf9c4ce0ef0ce32920483
delma <sdy2856@gmail.com>
2020-06-23 21:03:58 +0900

    [iOS]  feat :fire: 악세사리뷰에서 선택한 버튼에 따라 버튼 기능 구현
    #129

--- iOS/DoCollabo/DoCollabo/NewIssue/Controllers/NewIssueViewController.swift
+++ iOS/DoCollabo/DoCollabo/NewIssue/Controllers/NewIssueViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class NewIssueViewController: UIViewController {
-
+    
     @IBOutlet weak var titleHeaderView: TitleHeaderView!
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var markDownSegmentedControl: UISegmentedControl!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var newIssueAccessoryView: NewIssueAccessoryView!
+    
+    private var itemSelectionViewController: ItemSelectionViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +26,38 @@ class NewIssueViewController: UIViewController {
     
     private func configure() {
         configureUI()
+        configureSubViewController()
+        newIssueAccessoryView.delegate = self
     }
-
+    
     private func configureUI() {
         titleHeaderView.titleLabel.text = "새 이슈"
         titleHeaderView.changeUI(titleSize: 34, backgroundColor: .white)
         backgroundView.roundCorner(cornerRadius: 16.0)
         backgroundView.drawShadow(color: .black, offset: CGSize(width: 1, height: 1), radius: 4, opacity: 0.3)
+    }
+    
+    private func configureSubViewController() {
+        itemSelectionViewController = storyboard?.instantiateViewController(
+            identifier: String(describing: ItemSelectionViewController.self))
+    }
+}
+
+//MARK:- NewIssueAccessoryDelegate
+
+extension NewIssueViewController: NewIssueAccessoryDelegate {
+    func selectAssigneeButtonDidTap() {
+        present(itemSelectionViewController, animated: true)
+        itemSelectionViewController.fetchAssigneeStub()
+    }
+    
+    func selectLabelsButtonDidTap() {
+        present(itemSelectionViewController, animated: true)
+        itemSelectionViewController.fetchLabels()
+    }
+    
+    func selectMilestoneButtonDidTap() {
+        present(itemSelectionViewController, animated: true)
+        itemSelectionViewController.fetchMilestones()
     }
 }
