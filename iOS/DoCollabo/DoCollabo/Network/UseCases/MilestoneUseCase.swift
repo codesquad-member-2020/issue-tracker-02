d0cae594e4ff095c769a18114107c3c4b9260075
delma <sdy2856@gmail.com>
2020-06-23 21:00:32 +0900

    [iOS]  feat :fire: Milestone 관련한 EndPoint, UseCase 추가
    #129

---
+++ iOS/DoCollabo/DoCollabo/Network/UseCases/MilestoneUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  MilestoneUseCase.swift
+//  DoCollabo
+//
+//  Created by delma on 2020/06/23.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct MilestoneUseCase: UseCase {
+    var networkDispatcher: NetworkDispatcher = AF
+}
